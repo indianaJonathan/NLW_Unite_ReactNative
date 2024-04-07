dcf19704d68e988db60b0c24f2bcc4cf3270d811
Jonathan Hermam <jherman@outlook.com.br>
2024-04-07 18:25:39 -0300

    Adicionadas novas rotas para inscrição e credenciais

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,17 +1,30 @@
-import { View, Image } from "react-native";
-import { Link } from "expo-router";
+import { useState } from "react";
+
+import { View, Image, Alert } from "react-native";
+import { Link, router } from "expo-router";
 import { StatusBar } from "react-native";
 
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
-
 
 import { colors } from "@/styles/colors";
 
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
 
-
 export default function Register () {
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
+    function handleRegister () {
+        const errors: string[] = [];
+        if (!name.trim()) errors.push("Campo \"Nome\" é obrigatório");
+        if (!email.trim()) errors.push("Campo \"E-mail\" é obrigatório");
+
+        if (errors.length > 0) return Alert.alert("Inscrição", `Não foi possível realizar o cadastro devido aos seguintes erros:\n\n${errors.map((erro) => erro).join("\n")}`);
+
+        router.push("/ticket");
+    }
+    
     return (
         <View className="flex-1 bg-green-500 items-center justify-center p-8">
             <StatusBar barStyle="light-content" />
@@ -29,6 +42,7 @@ export default function Register () {
                     />
                     <Input.Field
                         placeholder="Nome completo"
+                        onChangeText={setName}
                     />
                 </Input>
                 <Input>
@@ -40,10 +54,12 @@ export default function Register () {
                     <Input.Field
                         placeholder="E-mail"
                         keyboardType="email-address"
+                        onChangeText={setEmail}
                     />
                 </Input>
                 <Button
                     title="Realizar inscrição"
+                    onPress={handleRegister}
                 />
                 <Link
                     href="/"
---
+++ src/components/credential.tsx
@@ -0,0 +1,75 @@
+import {
+    Text,
+    View,
+    Image,
+    ImageBackground,
+    TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+import { QRCode } from "@/components/qrcode";
+
+type CredentialProps = {
+    image?: string;
+    onChangeAvatar?: () => void;
+    onExpandQRCode?: () => void;
+}
+
+export function Credential ({ onChangeAvatar, onExpandQRCode, image }: CredentialProps) {
+    return (
+        <View className="w-full self-stretch items-center">
+            <Image
+                source={require("@/assets/ticket/band.png")}
+                className="w-24 h-52 z-10"
+            />
+            <View className="bg-black/20 self-stretch items-center pb-6 border border-white/10 mx-3 rounded-2xl -mt-5">
+                <ImageBackground
+                    source={require("@/assets/ticket/header.png")}
+                    className="px-6 py-8 h-40 items-center self-stretch border-b border-white/10 overflow-hidden"
+                >
+                    <View className="w-full flex-row items-center justify-between">
+                        <Text className="text-zinc-50 text-sm font-bold">
+                            Unite Summit
+                        </Text>
+                        <Text className="text-zinc-50 text-sm font-bold">
+                            #123
+                        </Text>
+                    </View>
+                    <View className="w-40 h-40 bg-black rounded-full" />
+                </ImageBackground>
+                <TouchableOpacity
+                    activeOpacity={0.9}
+                    onPress={onChangeAvatar}
+                >
+                    { image ?
+                        <Image
+                            source={{ uri: image }}
+                            className="w-36 h-36 rounded-full -mt-24"
+                        />
+                    :
+                        <View className="w-36 h-36 rounded-full -mt-24 items-center justify-center bg-gray-400">
+                            <Feather name="camera" color={colors.green[400]} size={32} />
+                        </View>
+                    }
+                </TouchableOpacity>
+                <Text className="font-bold text-2xl text-zinc-50 mt-4">
+                    Jonathan Hermam
+                </Text>
+                <Text className="font-regular text-base text-zinc-300 mb-4">
+                    jonathanhermam@gmail.com
+                </Text>
+                <QRCode value="teste" size={120}/>
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={onExpandQRCode}
+                >
+                    <View className="mt-6">
+                        <Text className="font-body text-orange-500 text-sm">
+                            Ampliar QRCode
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
