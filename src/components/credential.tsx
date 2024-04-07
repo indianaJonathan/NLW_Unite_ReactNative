15de243e745ddc6d6e0fc7cce0a27421242cfb55
Jonathan Hermam <jherman@outlook.com.br>
2024-04-07 20:46:53 -0300

    Adicionada integração com o back-end

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -4,20 +4,52 @@ import {
     Image,
     ImageBackground,
     TouchableOpacity,
+    useWindowDimensions,
 } from "react-native";
+
 import { Feather } from "@expo/vector-icons";
+import { MotiView } from "moti";
+
 import { colors } from "@/styles/colors";
+import { BadgeStore } from "@/store/badge-store";
+
 import { QRCode } from "@/components/qrcode";
 
 type CredentialProps = {
-    image?: string;
+    data: BadgeStore;
     onChangeAvatar?: () => void;
     onExpandQRCode?: () => void;
 }
 
-export function Credential ({ onChangeAvatar, onExpandQRCode, image }: CredentialProps) {
+export function Credential ({ onChangeAvatar, onExpandQRCode, data }: CredentialProps) {
+    const { height } = useWindowDimensions();
+
     return (
-        <View className="w-full self-stretch items-center">
+        <MotiView
+            className="w-full self-stretch items-center"
+            from={{
+                opacity: 0,
+                translateY: -height,
+                rotateZ: "50deg",
+                rotateY: "30deg",
+                rotateX: "30deg",
+            }}
+            animate={{
+                opacity: 1,
+                translateY: 0,
+                rotateZ: "0deg",
+                rotateY: "0deg",
+                rotateX: "0deg",
+            }}
+            transition={{
+                type: "spring",
+                damping: 20,
+                rotateZ: {
+                    damping: 15,
+                    mass: 3,
+                }
+            }}
+        >
             <Image
                 source={require("@/assets/ticket/band.png")}
                 className="w-24 h-52 z-10"
@@ -29,21 +61,21 @@ export function Credential ({ onChangeAvatar, onExpandQRCode, image }: Credentia
                 >
                     <View className="w-full flex-row items-center justify-between">
                         <Text className="text-zinc-50 text-sm font-bold">
-                            Unite Summit
+                            {data.eventTitle}
                         </Text>
                         <Text className="text-zinc-50 text-sm font-bold">
-                            #123
+                            #{data.id}
                         </Text>
                     </View>
-                    <View className="w-40 h-40 bg-black rounded-full" />
+                    <View className="w-44 h-44 bg-green-300 rounded-full" />
                 </ImageBackground>
                 <TouchableOpacity
                     activeOpacity={0.9}
                     onPress={onChangeAvatar}
                 >
-                    { image ?
+                    { data.image ?
                         <Image
-                            source={{ uri: image }}
+                            source={{ uri: data.image }}
                             className="w-36 h-36 rounded-full -mt-24"
                         />
                     :
@@ -53,12 +85,12 @@ export function Credential ({ onChangeAvatar, onExpandQRCode, image }: Credentia
                     }
                 </TouchableOpacity>
                 <Text className="font-bold text-2xl text-zinc-50 mt-4">
-                    Jonathan Hermam
+                    { data.name }
                 </Text>
                 <Text className="font-regular text-base text-zinc-300 mb-4">
-                    jonathanhermam@gmail.com
+                    { data.email }
                 </Text>
-                <QRCode value="teste" size={120}/>
+                <QRCode value={data.checkInURL} size={120}/>
                 <TouchableOpacity
                     activeOpacity={0.7}
                     onPress={onExpandQRCode}
@@ -70,6 +102,6 @@ export function Credential ({ onChangeAvatar, onExpandQRCode, image }: Credentia
                     </View>
                 </TouchableOpacity>
             </View>
-        </View>
+        </MotiView>
     );
 }
